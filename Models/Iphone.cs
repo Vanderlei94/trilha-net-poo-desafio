beef1562ead795d2a7b68691921ddccadece83db
Vanderlei94 <vanderleicas@gmail.com>
2024-10-04 19:38:36 +0100

    Projeto concluído

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,16 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
+        public Iphone(string modelo, string numero, string imei, int memoria ) : base(modelo, numero, imei, memoria){
+            
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o aplicativo {nomeApp} no iPhone.");
+        }
         // TODO: Sobrescrever o método "InstalarAplicativo"
     }
 }
--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,17 @@
+
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia : Smartphone
     {
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Nokia(string modelo, string numero, string imei, int memoria ) : base(modelo, numero, imei, memoria){
+
+        }
+        
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o aplicativo {nomeApp} no Nokia.");
+        }
     }
 }
